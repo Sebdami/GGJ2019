9b21b50510910025812c56824ab28f8ab771922d
Sébastien Damiens <sdamiens@oldskullgames.com>
2019-01-25 23:48:30 +0100

    Add PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 10f;
+
+    [SerializeField]
+    float gravity = 0.9f;
+
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+    }
+}
