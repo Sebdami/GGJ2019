1020ffd4ce9429c93bed011281f4832e13680b89
Sébastien Damiens <sdamiens@oldskullgames.com>
2019-01-27 03:32:31 +0100

    Update Menu, Fix Walk Anim, Add particles on color unlock

---
+++ Assets/Scripts/PlayerColorParticlesManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerColorParticlesManager : MonoBehaviour
+{
+    public ParticleSystem ps;
+    public Gradient[] Gradients;
+
+    void Start()
+    {
+        ParticleSystem.MainModule module = ps.main;
+        module.startColor = new ParticleSystem.MinMaxGradient(Gradients[GameManager.Instance.UnlockedColors]);
+        ps.Play();
+    }
+}
