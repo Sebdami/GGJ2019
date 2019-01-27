c6ea1577bf5bf34c09466bf664d2ecbf7c00e301
Sébastien Damiens <sdamiens@oldskullgames.com>
2019-01-27 04:50:54 +0100

    Add planetarium

---
+++ Assets/Cheat.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheat : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            GameManager.Instance.MinigameWin();
+        }
+    }
+}
