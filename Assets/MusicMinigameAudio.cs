4acdcf38edf03b3691983ac702c72ed429238caf
Sébastien Damiens <sdamiens@oldskullgames.com>
2019-01-27 13:50:21 +0100

    Add menu and game music

---
+++ Assets/MusicMinigameAudio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicMinigameAudio : MonoBehaviour
+{
+    private void Start()
+    {
+        AudioManager.Instance.PlayMusic(null);
+    }
+
+    private void OnDestroy()
+    {
+        AudioManager.Instance.PlayMusic(AudioManager.Instance.gameMusic);
+    }
+}
---
+++ Assets/PlayMusicOnlevelLoad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusicOnlevelLoad : MonoBehaviour
+{
+    public AudioClip music;
+    public bool Fade = true;
+    void Start()
+    {
+        if(Fade)
+            AudioManager.Instance.Fade(music, 1f);
+        else
+            AudioManager.Instance.PlayMusic(music);
+    }
+}
